ee9da23f4cf596d38953780ea1b6f1ff1f080f80
Anu-yadav923 <anuy8306@gmail.com>
2026-05-22 14:37:52 +0530

    added js

---
+++ java script/Notes_App/script.js
@@ -0,0 +1,51 @@
+const noteInput = document.getElementById("noteInput");
+
+const addBtn = document.getElementById("addBtn");
+
+const notesContainer = document.getElementById("notesContainer");
+
+addBtn.addEventListener("click", () => {
+    const notes = noteInput.value;
+    
+    const noteDiv = document.createElement("div");
+
+    noteDiv.classList.add("childnote")
+
+    // CREATE PARAGRAPH
+    const notePara = document.createElement("p");
+
+    notePara.textContent = notes;
+
+    if(notes == ""){
+       return alert("please fill a note")
+        
+    }
+
+    //  CREATE DELETE BUTTON
+
+    const deleteBtn = document.createElement("button");
+
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.classList.add("deleteBtn");
+
+    deleteBtn.addEventListener("click", () => {
+    noteDiv.remove();
+    });
+
+    // ADD ELEMENTS INSIDE NOTE DIV
+
+    noteDiv.appendChild(notePara);
+
+    noteDiv.appendChild(deleteBtn);
+
+    // ADD NOTE TO SCREEN
+
+    notesContainer.appendChild(noteDiv);
+
+    // CLEAR TEXTAREA
+    noteInput.value = "";
+});
+
+
+

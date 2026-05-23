0bbdb5d07646ea6159baaf4c525dac5ebf1622a1
Anu-yadav923 <anuy8306@gmail.com>
2026-05-23 13:47:17 +0530

    localStorage with UI rendering

--- java script/Notes_App/script.js
+++ java script/Notes_App/script.js
@@ -1,51 +1,67 @@
+let noteText = JSON.parse(localStorage.getItem("noteText")) || [];
+
 const noteInput = document.getElementById("noteInput");
 
 const addBtn = document.getElementById("addBtn");
 
 const notesContainer = document.getElementById("notesContainer");
 
+function renderNotetext() {
+    notesContainer.innerHTML ="" ;
+    noteText.forEach((note, index) => {
+        const noteDiv = document.createElement("div");
+        noteDiv.classList.add("childnote");
+
+        // CREATE PARAGRAPH
+        const notePara = document.createElement("p");
+        notePara.textContent = note;
+
+         //  CREATE DELETE BUTTON
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+        deleteBtn.classList.add("deleteBtn");
+        
+        deleteBtn.addEventListener("click", () => {
+    
+        noteText.splice(index, 1);
+
+        //saving updated notes array in localStorage
+
+        localStorage.setItem("noteText", JSON.stringify(noteText));
+
+       renderNotetext();
+    });
+
+         // ADD ELEMENTS INSIDE NOTE DIV
+        noteDiv.appendChild(notePara);
+
+        noteDiv.appendChild(deleteBtn);
+
+
+        // ADD NOTE TO SCREEN
+        notesContainer.appendChild(noteDiv);
+    });
+}
+
 addBtn.addEventListener("click", () => {
     const notes = noteInput.value;
-    
-    const noteDiv = document.createElement("div");
-
-    noteDiv.classList.add("childnote")
-
-    // CREATE PARAGRAPH
-    const notePara = document.createElement("p");
-
-    notePara.textContent = notes;
 
     if(notes == ""){
-       return alert("please fill a note")
+       return alert("please fill a note");
         
     }
 
-    //  CREATE DELETE BUTTON
+    noteText.push(notes);
 
-    const deleteBtn = document.createElement("button");
 
-    deleteBtn.textContent = "Delete";
+    localStorage.setItem("noteText", JSON.stringify(noteText));
 
-    deleteBtn.classList.add("deleteBtn");
-
-    deleteBtn.addEventListener("click", () => {
-    noteDiv.remove();
-    });
-
-    // ADD ELEMENTS INSIDE NOTE DIV
-
-    noteDiv.appendChild(notePara);
-
-    noteDiv.appendChild(deleteBtn);
-
-    // ADD NOTE TO SCREEN
-
-    notesContainer.appendChild(noteDiv);
-
+    renderNotetext();
     // CLEAR TEXTAREA
     noteInput.value = "";
 });
+
+renderNotetext();
 
 
 

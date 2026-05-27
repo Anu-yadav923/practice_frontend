7450b453e661e800098ca3b1f341db38f8c4411a
Anu-yadav923 <anuy8306@gmail.com>
2026-05-27 13:51:42 +0530

    form in react state

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,33 +1,36 @@
-import Productcard from "./Components/Productcard";
-
-const products = [
-  {
-    id: 1,
-    name: "iphone",
-    price: "$999"
-  },
-
-  {
-    id: 2,
-    name: "laptop",
-    price: "$1200"
-  }
-];
-
-
+import {useState} from "react";
 
 function App() {
-  return (
-    <>
-      {products.map((product) =>(
-        <Productcard
-          key={product.id}
-          name={product.name}
-          price={product.price}
-          
-          />
-      ))}
-    </>
+
+  const[email, setEmail] = useState("");
+  const[password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert("form submitted");
+  }
+
+  return(
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="email" placeholder="Enter Email"
+        onChange ={(e) => setEmail(e.target.value)} 
+        />
+        
+        <br></br>
+        <br></br>
+
+        <input type="password" placeholder="Enter password"
+        onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <br></br>
+        <br></br>
+
+        <button type="submit">login</button>
+
+      </form>
+    </div>
   );
 }
 

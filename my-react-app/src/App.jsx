8ecf4433f0fee3e8d17b5638e5b8c27e993d2247
Anu-yadav923 <anuy8306@gmail.com>
2026-06-09 11:21:00 +0530

    ScrollTrigger

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,22 +1,55 @@
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect} from "react";
+
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
 import gsap from "gsap";
 
 function App() {
+
   useEffect(() =>{
-    gsap.from(".card",{
+    const tl = gsap.timeline();
+    tl.add("start")
+
+    tl.from(".nav", {
+      y:-100,
+      opacity:0,
+      duration: 1
+    })
+
+    .from(".title", {
       y:100,
       opacity:0,
-      duration:1,
-      stagger:0.3
-    })
-  })
+      duration:1
+    }, "-=0.5")
 
-  return(
-    <div className="container">
-      <div className="card">card 2</div>
-      <div className="card">card 3</div>
-      <div className="card">card 1</div>
+    .from("btn", {
+      scale:0,
+      opacity:0,
+      duration:0.5
+    }, "start")
+
+    gsap.from(".box", {
+      y:100,
+      opacity:0,
+      duration:3,
+      ScrollTrigger:".box"
+    });
+  }, []);
+
+  return (
+    <div>
+      <div className="nav">Logo</div>
+
+      <h1 className="title"> Modern GSAP website</h1>
+
+      <button className="btn">Explore</button>
+
+      <div className="box">
+        box
+      </div>
     </div>
   );
 }
